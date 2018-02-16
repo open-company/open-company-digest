5a411fce2b16f96ae09a525f479fe30f9ad1fd3f
Sean Johnson <sean@snootymonkey.com>
2018-02-16 08:12:48 -0500

    Update README background blurb. Update dependencies.

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
     ;; Lisp on the JVM http://clojure.org/documentation
     [org.clojure/clojure "1.9.0"]
     [org.clojure/tools.cli "0.3.5"] ; commandline parsing https://github.com/clojure/tools.cli
-    [http-kit "2.3.0-alpha4"] ; Web client/server http://http-kit.org/
+    [http-kit "2.3.0-alpha5"] ; Web client/server http://http-kit.org/
     ;; Web application library https://github.com/ring-clojure/ring
     [ring/ring-devel "1.6.3"]
     ;; Web application library https://github.com/ring-clojure/ring
@@ -37,7 +37,7 @@
     [clojure.java-time "0.3.1"]
 
     ;; Library for OC projects https://github.com/open-company/open-company-lib
-    [open-company/lib "0.14.15"]
+    [open-company/lib "0.16.0"]
     ;; In addition to common functions, brings in the following common dependencies used by this project:
     ;; defun - Erlang-esque pattern matching for Clojure functions https://github.com/killme2008/defun
     ;; if-let - More than one binding for if/when macros https://github.com/LockedOn/if-let
@@ -96,7 +96,7 @@
       :plugins [
         ;; Check for code smells https://github.com/dakrone/lein-bikeshed
         ;; NB: org.clojure/tools.cli is pulled in by lein-kibit
-        [lein-bikeshed "0.5.0" :exclusions [org.clojure/tools.cli]] 
+        [lein-bikeshed "0.5.1" :exclusions [org.clojure/tools.cli]] 
         ;; Runs bikeshed, kibit and eastwood https://github.com/itang/lein-checkall
         [lein-checkall "0.1.1"]
         ;; pretty-print the lein project map https://github.com/technomancy/leiningen/tree/master/lein-pprint
