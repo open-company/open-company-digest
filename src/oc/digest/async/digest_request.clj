b528f43f3fc828115eb98f7e5900fbd0ce47801d
Nathan Zorn <nathan.zorn@gmail.com>
2018-12-14 14:17:03 -0500

    always use secure id url

--- src/oc/digest/async/digest_request.clj
+++ src/oc/digest/async/digest_request.clj
@@ -70,8 +70,8 @@
 
 ;; ----- Activity → Digest -----
 
-(defn- post-url [org-slug board-slug published-at id-token]
-  (str (s/join "/" [config/ui-server-url org-slug "all-posts"]) "?at=" published-at "&id=" id-token))
+(defn- post-url [org-slug board-slug secure-id id-token]
+  (str (s/join "/" [config/ui-server-url org-slug "post" secure-id]) "?id=" id-token))
 
 (defn- post [org-slug claims post]
   (let [comments (link-for "comments" post)
@@ -81,7 +81,7 @@
                          (assoc :secure-uuid (:secure-uuid post)))
         id-token (jwt/generate-id-token token-claims config/passphrase)]
     {:headline (:headline post)
-     :url (post-url org-slug (:board-slug post) (:published-at post) id-token)
+     :url (post-url org-slug (:board-slug post) (:secure-uuid post) id-token)
      :publisher (:publisher post)
      :published-at (:published-at post)
      :comment-count (or (:count comments) 0)
