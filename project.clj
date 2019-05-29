b5eec9eaa9b7eac9810b648db059f8ef3186f12c
Sean Johnson <sean@snootymonkey.com>
2019-05-29 10:26:27 -0400

    Update dependencies.

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
 
   :dependencies [
     ;; Lisp on the JVM http://clojure.org/documentation
-    [org.clojure/clojure "1.10.1-beta2"]
+    [org.clojure/clojure "1.10.1-RC1"]
     [org.clojure/tools.cli "0.4.2"] ; commandline parsing https://github.com/clojure/tools.cli
     [http-kit "2.4.0-alpha4"] ; Web client/server http://http-kit.org/
     ;; Web application library https://github.com/ring-clojure/ring
@@ -30,7 +30,7 @@
     ;; Web routing https://github.com/weavejester/compojure
     [compojure "1.6.1"]
     ;; HTTP client https://github.com/dakrone/clj-http
-    [clj-http "3.9.1"]
+    [clj-http "3.10.0"]
     ;; Simple scheduler https://github.com/juxt/tick
     ;; NB: Don't upgrade to +0.4, it's not backward compatible as timeline/clock/schedele namespaces all deprecated
     [tick "0.3.5"]
@@ -38,7 +38,7 @@
     [clojure.java-time "0.3.2"]
 
     ;; Library for OC projects https://github.com/open-company/open-company-lib
-    [open-company/lib "0.17.5"]
+    [open-company/lib "0.17.9"]
     ;; In addition to common functions, brings in the following common dependencies used by this project:
     ;; defun - Erlang-esque pattern matching for Clojure functions https://github.com/killme2008/defun
     ;; if-let - More than one binding for if/when macros https://github.com/LockedOn/if-let
