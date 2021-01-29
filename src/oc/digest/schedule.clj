7f20288972ffc7e9c3ce776a54923f3073eac3ab
bago2k4 <iacopo@rc0.eu>
2021-01-29 19:37:13 +0100

    Do not report all the failure during digest, report only the last after all retries attempts.

--- src/oc/digest/schedule.clj
+++ src/oc/digest/schedule.clj
@@ -53,18 +53,20 @@
                           (lib-schema/valid? DigestSent sent-map))]
          (user-res/last-digest-at! conn (:user-id user) (:org-uuid sent-map) (:start sent-map))))
      (catch Exception e
-       (let [err-msg (str "Digest failed for user: " (:user-id user) " (retry " retry ")")]
-        (timbre/warn err-msg)
-        (sentry/capture {:message {:message err-msg}
-                         :throwable e
-                         :extra {:retry retry
-                                 :user-id (:user-id user)
-                                 :user user}})
-        ;; Retry if we got an error after 1 second...
-        (when (and (int? retry)
-                    (<= retry max-retry))
+      ;; Retry if we got an error after 1 second...
+      (if (and (int? retry)
+               (<= retry max-retry))
+        (do
           (Thread/sleep 1000)
-          (digest-for conn user skip-send? (inc retry))))))))
+          (digest-for conn user skip-send? (inc retry)))
+        ;; NB DO NOT re-throw the error or the following users won't get the digest
+        (let [err-msg (str "Digest failed for user: " (:user-id user) " (retry " retry ")")]
+          (timbre/warn err-msg)
+          (sentry/capture {:message {:message err-msg}
+                          :throwable e
+                          :extra {:retry retry
+                                  :user-id (:user-id user)
+                                  :user user}})))))))
 
 (defun digest-run
 
