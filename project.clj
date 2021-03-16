092bfde53162f9867d3d7f5aa33bd87c52b46aa1
bago2k4 <iacopo@rc0.eu>
2021-03-16 16:42:13 +0100

    Update sentry-clj in lib imports comment.

--- project.clj
+++ project.clj
@@ -53,7 +53,7 @@
     ;; Schema - Data validation https://github.com/Prismatic/schema
     ;; Timbre - Pure Clojure/Script logging library https://github.com/ptaoussanis/timbre
     ;; Amazonica - A comprehensive Clojure client for the AWS API. https://github.com/mcohen01/amazonica
-    ;; Raven - Interface to Sentry error reporting https://github.com/sethtrain/raven-clj
+    ;; sentry-clj - Interface to Sentry error reporting https://github.com/getsentry/sentry-clj
     ;; Cheshire - JSON encoding / decoding https://github.com/dakrone/cheshire
     ;; clj-jwt - A Clojure library for JSON Web Token(JWT) https://github.com/liquidz/clj-jwt
     ;; clj-time - Date and time lib https://github.com/clj-time/clj-time
